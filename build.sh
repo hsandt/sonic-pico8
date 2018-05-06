36f9abffa79d53a175bb05143dca80fd98d07eed
hsandt <hs@fuwar.in>
2018-05-06 19:43:39 +0200

    [BUILD] Improved build command to reload if pico8 is already running

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ mkdir -p build
 # build the game from the different modules
 p8tool build --lua "src/main.lua" --lua-path="?.lua;$(pwd)/src/?.lua" --gfx "data/data.p8" --gff "data/data.p8" --map "data/data.p8" --sfx "data/data.p8" --music "data/data.p8" "build/game.p8" &&
 # replace non-ascii glyphs from special codes
-python3.6 postbuild/replace_glyphs.py "build/game.p8"
+python3.6 postbuild/replace_glyphs.py "build/game.p8" &&
+echo "Build succeeded: build/game.p8" ||
+(echo "Build failed: build/game.p8" && exit 1)
---
+++ reload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# --sync makes sure the window is active before sending the key, but it gets stuck
+# if no matching window is found, so timeout makes sure it doesn't happen
+timeout 0.1 xdotool search --sync --class pico8 windowactivate key ctrl+r &&
+echo "Reloaded pico8"

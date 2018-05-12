6ef64c3c331163fd05bf0ee959506ee7b247b36b
hsandt <hs@fuwar.in>
2018-05-12 20:00:22 +0200

    [CAMERA] Refactored camera offset and render environment

--- src/stage.lua
+++ src/stage.lua
@@ -41,19 +41,15 @@ end
 function stage_state:render()
   cls()
 
-  -- background
   camera()
+  -- background
   rectfill(0, 0, 127, 127, colors.dark_purple)
+  -- debug label
   print("stage state", 3*11, 1*12)
 
   -- update camera offset
-  camera(self.camera_position.x - screen_width / 2, self.camera_position.y - screen_height / 2)
-
-  -- dummy tiles
-  rectfill(0, 0, 7, 7, colors.pink)
-  rectfill(8, 8, 15, 15, colors.pink)
-
-  -- player character
+  self:set_camera_offset_stage()
+  self:render_environment()
   self:render_player_character()
 end
 
@@ -91,11 +87,19 @@ end
 
 -- camera
 
+-- update camera position based on player character position
 function stage_state:update_camera()
-  -- following player character with stiff motion
+  -- stiff motion
   self.camera_position.x = self.player_character.position.x
   self.camera_position.y = self.player_character.position.y
 end
+
+-- set the camera offset for stage elements
+function stage_state:set_camera_offset_stage()
+  -- the camera position is used to render the stage, subtracting a half screen to center the view
+  camera(self.camera_position.x - screen_width / 2, self.camera_position.y - screen_height / 2)
+end
+
 
 -- render
 
@@ -103,6 +107,14 @@ end
 function stage_state:render_player_character()
   self.player_character:render()
 end
+
+-- render the stage environment (tiles)
+function stage_state:render_environment()
+  -- dummy tiles
+  rectfill(0, 0, 7, 7, colors.pink)
+  rectfill(8, 8, 15, 15, colors.pink)
+end
+
 
 -- export
 

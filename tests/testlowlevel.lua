3f8170eefa0e7c0d9642357460dcc02cff1d1f8c
hsandt <hs@fuwar.in>
2018-05-13 01:21:11 +0200

    [TEST] Split testall in testlowlevel and testgamestates
    testall is too massive, doesn't work with pico-test anymore

---
+++ tests/testlowlevel.lua
@@ -0,0 +1,24 @@
+picotest = require("picotest")
+
+require("testclass")
+require("testcoroutine")
+require("testhelper")
+require("testmath")
+require("testplayercharacter")
+require("testsprite")
+
+function run_all_tests()
+  picotest.test('all', function(desc,it)
+    for test_callback in all(picotest.test_suite) do
+      test_callback(desc,it)
+    end
+  end)
+end
+
+function _init()
+  run_all_tests()
+end
+
+-- empty update allows to close test window with ctrl+c on success
+function _update()
+end

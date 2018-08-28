b1b4104388c1b8e0aa368943b3f66c826d28ad30
huulong <n.huu.long@gmail.com>
2018-08-29 01:53:37 +0200

    [TEST] Added test for unittest_helper, fixed some typo

--- src/engine/test/unittest_helper.lua
+++ src/engine/test/unittest_helper.lua
@@ -8,6 +8,6 @@ function are_same_with_message(t, passed)
     -- passed is not same as t, return false with does_not_contain message (will appear when using assert(not are_same(...)))
     return true, "Expected objects to not be the same.\nPassed in:\n"..nice_dump(passed).."\nDid not expect:\n"..nice_dump(t)
   else
-    return false, "Expected object to be the same.\nPassed in:\n"..nice_dump(passed).."\nExpected:\n"..nice_dump(t)
+    return false, "Expected objects to be the same.\nPassed in:\n"..nice_dump(passed).."\nExpected:\n"..nice_dump(t)
   end
 end

aca90aacd72fd02860520632cc685d54163a787d
huulong <n.huu.long@gmail.com>
2018-08-28 20:37:38 +0200

    [DATA] Added collision_data.lua for tile data

---
+++ src/game/data/collision_data.lua
@@ -0,0 +1,6 @@
+-- table mapping tile sprite id locations to tile collision masks
+-- this will be completed as tiles are added, adding extra information
+--  such as mirror-y: bool for upside-down tiles
+sprite_to_collision_masks = {
+  [sprite_id_location(0, 4)] = sprite_id_location(0, 5)
+}

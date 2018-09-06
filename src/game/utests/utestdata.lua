d628e3a4352e1acbd50524c6ebc1a9afe63aebff
huulong <n.huu.long@gmail.com>
2018-09-06 21:32:08 +0200

    [TEST] Added collision data test in utestdata

--- src/game/utests/utestdata.lua
+++ src/game/utests/utestdata.lua
@@ -1,9 +1,20 @@
 require("engine/test/unittest")
 require("engine/core/math")
 local collision = require("engine/physics/collision")
+local collision_data = require("game/data/collision_data")
 local height_array = collision.height_array
 
-check('. height_array._fill_array should fill the array with tile mask data: full', function ()
+check('sprite_id_location(0, 4) should have collision flag set', function ()
+  local sprite_id = sprite_id_location(0, 4):to_sprite_id()
+  assert(fget(sprite_id, sprite_flags.collision), "sprite_id_location(0, 4) has collision flag unset")
+end)
+
+check('sprite_id_location(0, 4) should have collision mask id set to location below', function ()
+  local sprite_id = sprite_id_location(0, 4):to_sprite_id()
+  assert(collision_data.sprite_id_to_collision_mask_id_locations[sprite_id] == sprite_id_location(0, 5))
+end)
+
+check('. height_array._fill_array on sprite_id_location(0, 5) should fill the array with tile mask data: full', function ()
   local array = {}
   height_array._fill_array(array, sprite_id_location(0, 5))
   assert(are_same_with_message({8, 8, 8, 8, 8, 8, 8, 8}, array))

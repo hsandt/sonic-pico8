9b7d01edd0a5879d0a19ebdd6e6153e9272f1f9c
hsandt <hs@fuwar.in>
2018-09-27 18:19:30 +0200

    [STAGE] Fixed stage slope data, added gap in level 1

--- src/game/itests/itestplayercharacter.lua
+++ src/game/itests/itestplayercharacter.lua
@@ -711,13 +711,7 @@ itest.final_assertion = function ()
 
   return success, final_message
 end
--- to compute position x from x0 after n frames at accel a from speed s0: x = x0 + n*s0 + n(n+1)/2*a
--- to compute speed s from s0 after n frames at accel a: x = s0 + n*a
--- character will be blocked when right wall sensor is at x = 16, so when center is at x = 12
--- at frame 1: pos (4 + 0.0234375, 80), velocity (0.0234375, 0), grounded
--- at frame 29: pos (13.515625, 80), velocity (0.6796875, 0), about to meet wall
--- at frame 30: pos (14, 80), velocity (0, 0), stopped at wall, position clamped and speed reset to 0
-itest = integration_test('#solo platformer ground wall block right', {stage.state.type})
+itest = integration_test('platformer ground wall block right', {stage.state.type})
 itest_manager:register(itest)
 
 itest.setup = function ()
@@ -742,8 +736,14 @@ itest.teardown = function ()
   teardown_map_data()
 end
 
--- wait 30 frames and stop
 
+-- wait 29 frames and stop
+-- to compute position x from x0 after n frames at accel a from speed s0: x = x0 + n*s0 + n(n+1)/2*a
+-- to compute speed s from s0 after n frames at accel a: x = s0 + n*a
+-- character will be blocked when right wall sensor is at x = 16, so when center is at x = 12
+-- at frame 1: pos (4 + 0.0234375, 80), velocity (0.0234375, 0), grounded
+-- at frame 29: pos (13.515625, 80), velocity (0.6796875, 0), about to meet wall
+-- at frame 30: pos (14, 80), velocity (0, 0), stopped at wall, position clamped and speed reset to 0
 itest:add_action(time_trigger(29, true), function () end)
 
 -- check that player char has moved to the right and is still on the ground
@@ -751,6 +751,76 @@ itest.final_assertion = function ()
   local is_motion_state_expected, motion_state_message = motion_states.grounded == stage.state.player_character.motion_state, "Expected motion state 'grounded', got "..stage.state.player_character.motion_state
   -- to compute position x from x0 after n frames at accel a from speed s0: x = x0 + n*s0 + n(n+1)/2*a
   local is_position_expected, position_message = almost_eq_with_message(vector(14., 80.), stage.state.player_character:get_bottom_center(), 1/256)
+  -- to compute speed s from s0 after n frames at accel a: x = s0 + n*a
+  local is_ground_speed_expected, ground_speed_message = almost_eq_with_message(0, stage.state.player_character.ground_speed_frame, 1/256)
+  local is_velocity_expected, velocity_message = almost_eq_with_message(vector(0, 0), stage.state.player_character.velocity_frame, 1/256)
+
+  local final_message = ""
+
+  local success = is_position_expected and is_ground_speed_expected and is_velocity_expected and is_motion_state_expected
+  if not success then
+    if not is_motion_state_expected then
+      final_message = final_message..motion_state_message.."\n"
+    end
+    if not is_position_expected then
+      final_message = final_message..position_message.."\n"
+    end
+    if not is_ground_speed_expected then
+      final_message = final_message..ground_speed_message.."\n"
+    end
+    if not is_velocity_expected then
+      final_message = final_message..velocity_message.."\n"
+    end
+
+  end
+
+  return success, final_message
+end
+
+
+itest = integration_test('platformer slope wall block right', {stage.state.type})
+itest_manager:register(itest)
+
+itest.setup = function ()
+  setup_map_data()
+
+  mset(0, 10, 64)  -- to walk on
+  mset(1, 10, 64)  -- support ground for slope
+  mset(1,  9, 65)  -- slope to walk on
+  mset(2,  8, 64)  -- blocking wall at the top of the slope
+
+  flow:change_gamestate_by_type(stage.state.type)
+
+  -- respawn character on the ground (important to always start with grounded state)
+  stage.state.player_character:spawn_at(vector(4., 80. - playercharacter_data.center_height_standing))  -- set bottom y at 80
+  stage.state.player_character.control_mode = control_modes.puppet
+  stage.state.player_character.motion_mode = motion_modes.platformer
+
+  -- start moving to the right from frame 0 by setting intention in setup
+  stage.state.player_character.move_intention = vector(1, 0)
+end
+
+itest.teardown = function ()
+  teardown_map_data()
+end
+
+-- wait 29 frames and stop
+
+-- to compute position x from x0 after n frames at accel a from speed s0: x = x0 + n*s0 + n(n+1)/2*a
+-- to compute speed s from s0 after n frames at accel a: x = s0 + n*a
+-- character will be blocked when right wall sensor is at x = 16, so when center is at x = 12
+-- at frame 1: pos (4 + 0.0234375, 80), velocity (0.0234375, 0), grounded
+-- at frame 29: pos (13.515625, 80 - 8), velocity (0.6796875, 0), about to meet wall
+-- at frame 30: pos (14, 80 - 8), velocity (0, 0), stopped at wall, position clamped and speed reset to 0
+-- note that speed decrease on slope is not implemented yet (via cosine but also gravity), so this test will have to change when it is
+--  when it is, prefer passing a very low slope or apply slope factor to adapt the position/velocity calculation
+itest:add_action(time_trigger(29, true), function () end)
+
+-- check that player char has moved to the right and is still on the ground
+itest.final_assertion = function ()
+  local is_motion_state_expected, motion_state_message = motion_states.grounded == stage.state.player_character.motion_state, "Expected motion state 'grounded', got "..stage.state.player_character.motion_state
+  -- to compute position x from x0 after n frames at accel a from speed s0: x = x0 + n*s0 + n(n+1)/2*a
+  local is_position_expected, position_message = almost_eq_with_message(vector(14, 80 - 8), stage.state.player_character:get_bottom_center(), 1/256)
   -- to compute speed s from s0 after n frames at accel a: x = s0 + n*a
   local is_ground_speed_expected, ground_speed_message = almost_eq_with_message(0, stage.state.player_character.ground_speed_frame, 1/256)
   local is_velocity_expected, velocity_message = almost_eq_with_message(vector(0, 0), stage.state.player_character.velocity_frame, 1/256)

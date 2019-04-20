c9fd99d1ef4278a41b04a100a43ebafd4db39909
hsandt <hs@fuwar.in>
2019-04-20 02:48:48 +0100

    [SPRITE] Use animation data table instead of single animation for animated_sprite

--- src/engine/render/animated_sprite.lua
+++ src/engine/render/animated_sprite.lua
@@ -1,47 +1,61 @@
--- stateful animated sprite compounded of an animated_sprite_data and an animation state
--- can be used as component of an object rendered with some animation
--- data can be swept for another data to easily switch sprite animation
+-- stateful animated sprite compounded of an animated_sprite_data table and an animation state
+-- it can be used as component of an object rendered with some animation
+-- for objects with a single animation, use a data table containing a single element
 animated_sprite = new_class()
 
--- data          animated_sprite_data  associated data
--- playing       bool                  is the animation playing?
--- current_step  int                   index of the current sprite shown in the animation sequence, starting at 1
--- local_frame   int                   current frame inside the current step, starting at 0
-function animated_sprite:_init(data)
-  self.data = data
-  self.playing = true
-  self.current_step = 1
-  self.local_frame = 0
+-- data_table        {string: animated_sprite_data}  table of animated sprite data, indexed by animation key (unique name)
+-- playing           bool                            is the animation playing? false if the animation has reached the end and stopped
+-- current_anim_key  string|nil                      key in data_table of animation currently played / paused, or nil if no animation is set at all
+-- current_step      int|nil                         index of the current sprite shown in the animation sequence, starting at 1, or nil if no animation is set at all
+-- local_frame       int|nil                         current frame inside the current step, starting at 0, or nil if no animation is set at all
+function animated_sprite:_init(data_table)
+  self.data_table = data_table
+  self.playing = false
+  self.current_anim_key = nil  -- the sprite will be invisible until we start an animation
+  self.current_step = nil
+  self.local_frame = nil
 end
 
 --#if log
 function animated_sprite:_tostring()
-  return "animated_sprite("..joinstr(", ", self.data, self.playing, self.current_step, self.local_frame)..")"
+  return "animated_sprite("..joinstr(", ", nice_dump(self.data_table, true), self.playing, self.current_anim_key, self.current_step, self.local_frame)..")"
 end
 --#endif
+
+-- start animation with given key: string
+function animated_sprite:play(anim_key)
+  assert(self.data_table[anim_key] ~= nil, "animated_sprite:play: self.data_table['"..anim_key.."'] doesn't exist")
+  self.playing = true
+  self.current_anim_key = anim_key
+  self.current_step = 1
+  self.local_frame = 0
+end
 
 -- update the sprite animation
 -- this must be called once per update at 60 fps, before the render phase
 function animated_sprite:update()
-  -- check if we have reached the end of this step
-  if self.local_frame + 1 < self.data.step_frames then
-    -- keep same sprite and increment local frame counter
-    self.local_frame = self.local_frame + 1
-  else
-    -- end of step reached, check if there is another sprite afterward
-    if self.current_step < #self.data.sprites then
-      -- show next sprite and reset local frame counter
-      self.current_step = self.current_step + 1
-      self.local_frame = 0
+  if self.playing then
+    local anim_spr_data = self.data_table[self.current_anim_key]
+    -- check if we have reached the end of this step
+    if self.local_frame + 1 < anim_spr_data.step_frames then
+      -- keep same sprite and increment local frame counter
+      self.local_frame = self.local_frame + 1
     else
-      -- end of last step reached, should we loop?
-      if self.data.looping then
-        -- continue playing from start
-        self.current_step = 1
+      -- end of step reached, check if there is another sprite afterward
+      if self.current_step < #anim_spr_data.sprites then
+        -- show next sprite and reset local frame counter
+        self.current_step = self.current_step + 1
         self.local_frame = 0
       else
-        -- stop playing
-        self.playing = false
+        -- end of last step reached, should we loop?
+        if anim_spr_data.looping then
+          -- continue playing from start
+          self.current_step = 1
+          self.local_frame = 0
+        else
+          -- stop playing
+          self.playing = false
+        end
       end
     end
   end
@@ -49,10 +63,13 @@ end
 
 -- render the current sprite data with passed arguments
 function animated_sprite:render(position, flip_x, flip_y)
-  local current_sprite_data = self.data.sprites[self.current_step]
-  current_sprite_data:render(position, flip_x, flip_y)
+  if self.current_anim_key then
+    -- an animation is set, render even if not playing since we want to show the last frame
+    --   of a non-looped anim as a still frame
+    local anim_spr_data = self.data_table[self.current_anim_key]
+    local current_sprite_data = anim_spr_data.sprites[self.current_step]
+    current_sprite_data:render(position, flip_x, flip_y)
+  end
 end
-
-
 
 return animated_sprite
--- tests/utestanimated_sprite.lua
+++ tests/utestanimated_sprite.lua
@@ -8,32 +8,70 @@ describe('animated_sprite', function ()
   local spr_data1 = sprite_data(sprite_id_location(1, 0), tile_vector(1, 2), vector(4, 6))
   local spr_data2 = sprite_data(sprite_id_location(2, 0), tile_vector(1, 2), vector(4, 6))
   local anim_spr_data = animated_sprite_data({spr_data1, spr_data2}, 10, true)
-  local anim_spr_data_once = animated_sprite_data({spr_data1, spr_data2}, 10, false)
+  local anim_spr_data_no_loop = animated_sprite_data({spr_data1, spr_data2}, 10, false)
+  local anim_spr_data_table = {
+    loop = anim_spr_data,
+    no_loop = anim_spr_data_no_loop
+  }
 
   describe('_init', function ()
     it('should init an animated sprite with data, automatically playing from step 1, frame 0', function ()
-      local anim_spr = animated_sprite(anim_spr_data)
-      assert.are_same({anim_spr_data, true, 1, 0},
-        {anim_spr.data, anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      assert.are_same({anim_spr_data_table, false, nil, nil, nil},
+        {anim_spr.data_table, anim_spr.playing, anim_spr.current_anim_key, anim_spr.current_step, anim_spr.local_frame})
     end)
   end)
 
   describe('_tostring', function ()
 
     it('should return a string describing data, current step and local frame', function ()
-      local anim_spr = animated_sprite(anim_spr_data)
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = true
+      anim_spr.current_anim_key = "idle"
       anim_spr.current_step = 2
       anim_spr.local_frame = 5
-      assert.are_equal("animated_sprite(animated_sprite_data([2 sprites], 10, true), true, 2, 5)", anim_spr:_tostring())
+      assert.are_equal("animated_sprite({loop = animated_sprite_data([2 sprites], 10, true), no_loop = animated_sprite_data([2 sprites], 10, false)}, true, idle, 2, 5)", anim_spr:_tostring())
+    end)
+
+  end)
+
+  describe('play', function ()
+
+    it('should assert if the anim_key is not found', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+
+      assert.has_error(function() anim_spr:play("unknown") end,
+        "animated_sprite:play: self.data_table['unknown'] doesn't exist")
+    end)
+
+    it('should start playing the anim from the first step, first frame', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr:play("loop")
+
+      assert.are_same({true, "loop", 1, 0},
+        {anim_spr.playing, anim_spr.current_anim_key, anim_spr.current_step, anim_spr.local_frame})
     end)
 
   end)
 
   describe('update', function ()
 
+    it('should do nothing when not playing', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = false
+      anim_spr.current_step = 9
+      anim_spr.local_frame = 99
+
+      anim_spr:update()
+
+      assert.are_same({false, 9, 99},
+        {anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
+    end)
+
     it('should increment the local frame if under the animation step_frames', function ()
-      local anim_spr = animated_sprite(anim_spr_data)
+      local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = true
+      anim_spr.current_anim_key = "loop"
       anim_spr.current_step = 1
       anim_spr.local_frame = 8  -- data.step_frames is 10, so frames play from 0 to 9
 
@@ -44,8 +82,9 @@ describe('animated_sprite', function ()
     end)
 
     it('should reset local frame and enter next step when step_frames is reached', function ()
-      local anim_spr = animated_sprite(anim_spr_data)
+      local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = true
+      anim_spr.current_anim_key = "loop"
       anim_spr.current_step = 1
       anim_spr.local_frame = 9  -- data.step_frames - 1
 
@@ -56,8 +95,9 @@ describe('animated_sprite', function ()
     end)
 
     it('should continue playing from the start when looping and end of animation has been reached', function ()
-      local anim_spr = animated_sprite(anim_spr_data)
+      local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = true
+      anim_spr.current_anim_key = "loop"
       anim_spr.current_step = 2
       anim_spr.local_frame = 9  -- data.step_frames - 1
 
@@ -68,7 +108,8 @@ describe('animated_sprite', function ()
     end)
 
     it('should stop playing when not looping and end of animation has been reached', function ()
-      local anim_spr = animated_sprite(anim_spr_data_once)
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.current_anim_key = "no_loop"
       anim_spr.playing = true
       anim_spr.current_step = 2
       anim_spr.local_frame = 9  -- data.step_frames - 1
@@ -83,13 +124,8 @@ describe('animated_sprite', function ()
 
   describe('render', function ()
 
-    local anim_spr = animated_sprite(anim_spr_data)
-
     setup(function ()
       sprite_data_render = stub(sprite_data, "render")
-
-      anim_spr.current_step = 2
-      anim_spr.local_frame = 5
     end)
 
     teardown(function ()
@@ -100,7 +136,22 @@ describe('animated_sprite', function ()
       sprite_data_render:clear()
     end)
 
-    it('should render the sprite from the id location, at the draw position minus pivot, with correct span when not flipping', function ()
+    it('should not render the sprite when not playing', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.current_anim_key = nil
+
+      anim_spr:render(vector(41, 80), false, true)
+
+      assert.spy(sprite_data_render).was_called(0)
+    end)
+
+    it('should render the sprite for current animation and step, with passed arguments', function ()
+
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.current_anim_key = "no_loop"
+      anim_spr.current_step = 2
+      anim_spr.local_frame = 5
+
       anim_spr:render(vector(41, 80), false, true)
 
       assert.spy(sprite_data_render).was_called(1)

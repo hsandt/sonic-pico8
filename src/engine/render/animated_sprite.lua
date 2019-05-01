47771be90654467d9a3c5e5f74fd2b5bff3666c9
hsandt <hs@fuwar.in>
2019-05-02 00:52:15 +0100

    [ANIM] Added playback speed

--- src/engine/render/animated_sprite.lua
+++ src/engine/render/animated_sprite.lua
@@ -5,12 +5,15 @@ animated_sprite = new_class()
 
 -- data_table        {string: animated_sprite_data}  table of animated sprite data, indexed by animation key (unique name)
 -- playing           bool                            is the animation playing? false if the animation has reached the end and stopped
+-- play_speed_frame  float > 0                       playback speed multiplier (in frames per update). it's a float so fractions of frames may be advanced every frame
 -- current_anim_key  string|nil                      key in data_table of animation currently played / paused, or nil if no animation is set at all
 -- current_step      int|nil                         index of the current sprite shown in the animation sequence, starting at 1, or nil if no animation is set at all
--- local_frame       int|nil                         current frame inside the current step, starting at 0, or nil if no animation is set at all
+-- local_frame       float|nil                       current frame inside the current step, starting at 0, or nil if no animation is set at all
+--                                                   since play_speed_frame is a float, local_frame is also a float to allow fractional advance
 function animated_sprite:_init(data_table)
   self.data_table = data_table
   self.playing = false
+  self.play_speed_frame = 0.
   self.current_anim_key = nil  -- the sprite will be invisible until we start an animation
   self.current_step = nil
   self.local_frame = nil
@@ -18,23 +21,28 @@ end
 
 --#if log
 function animated_sprite:_tostring()
-  return "animated_sprite("..joinstr(", ", nice_dump(self.data_table, true), self.playing, self.current_anim_key, self.current_step, self.local_frame)..")"
+  return "animated_sprite("..joinstr(", ", nice_dump(self.data_table, true), self.playing, self.play_speed_frame, self.current_anim_key, self.current_step, self.local_frame)..")"
 end
 --#endif
 
--- play animation with given key: string
+-- play animation with given key: string at playback speed: float (default: 1.)
 -- if this animation is not already set, play it from start
 -- if this animation is already set, check from_start:
 -- - if true, play it from start
 -- - if false, do nothing (if playing, it means continuing to play; if not playing (e.g. stopped at the end), do not replay from start)
 --   note that even if the animation is paused, it won't be resumed in this case (because we don't have a flag has_ended to distinguish pause and end)
 -- by default, continue animation already playing
-function animated_sprite:play(anim_key, from_start)
+function animated_sprite:play(anim_key, from_start, speed)
   assert(self.data_table[anim_key] ~= nil, "animated_sprite:play: self.data_table['"..anim_key.."'] doesn't exist")
 
   if from_start == nil then
     from_start = false
   end
+
+  speed = speed or 1.
+
+  -- always update speed. this is useful to change anim speed while continue playing the same animation
+  self.play_speed = speed
 
   if self.current_anim_key ~= anim_key or from_start then
     self.playing = true               -- this will do nothing if forcing replay from start during play
@@ -46,28 +54,31 @@ end
 
 -- update the sprite animation
 -- this must be called once per update at 60 fps, before the render phase
+-- fractional playback speed is supported, but not negative playback
 function animated_sprite:update()
   if self.playing then
     local anim_spr_data = self.data_table[self.current_anim_key]
+    -- advance by playback speed
+    self.local_frame = self.local_frame + self.play_speed_frame
     -- check if we have reached the end of this step
-    if self.local_frame + 1 < anim_spr_data.step_frames then
-      -- keep same sprite and increment local frame counter
-      self.local_frame = self.local_frame + 1
-    else
+    -- in case the playback speed is so high we will skip frames,
+    --   continue checking until time remainder is less than a step duration
+    while self.local_frame >= anim_spr_data.step_frames do
       -- end of step reached, check if there is another sprite afterward
       if self.current_step < #anim_spr_data.sprites then
         -- show next sprite and reset local frame counter
         self.current_step = self.current_step + 1
-        self.local_frame = 0
+        self.local_frame = self.local_frame - anim_spr_data.step_frames
       else
         -- end of last step reached, should we loop?
         if anim_spr_data.looping then
           -- continue playing from start
           self.current_step = 1
-          self.local_frame = 0
+          self.local_frame = self.local_frame - anim_spr_data.step_frames
         else
           -- stop playing
           self.playing = false
+          break
         end
       end
     end
--- src/engine/tests/utestanimated_sprite.lua
+++ src/engine/tests/utestanimated_sprite.lua
@@ -7,7 +7,7 @@ describe('animated_sprite', function ()
 
   local spr_data1 = sprite_data(sprite_id_location(1, 0), tile_vector(1, 2), vector(4, 6))
   local spr_data2 = sprite_data(sprite_id_location(2, 0), tile_vector(1, 2), vector(4, 6))
-  local anim_spr_data = animated_sprite_data({spr_data1, spr_data2}, 10, true)
+  local anim_spr_data = animated_sprite_data({spr_data1, spr_data2, spr_data1}, 10, true)
   local anim_spr_data_no_loop = animated_sprite_data({spr_data1, spr_data2}, 10, false)
   local anim_spr_data_table = {
     loop = anim_spr_data,
@@ -17,8 +17,8 @@ describe('animated_sprite', function ()
   describe('_init', function ()
     it('should init an animated sprite with data, automatically playing from step 1, frame 0', function ()
       local anim_spr = animated_sprite(anim_spr_data_table)
-      assert.are_same({anim_spr_data_table, false, nil, nil, nil},
-        {anim_spr.data_table, anim_spr.playing, anim_spr.current_anim_key, anim_spr.current_step, anim_spr.local_frame})
+      assert.are_same({anim_spr_data_table, false, 0., nil, nil, nil},
+        {anim_spr.data_table, anim_spr.playing, anim_spr.play_speed_frame, anim_spr.current_anim_key, anim_spr.current_step, anim_spr.local_frame})
     end)
   end)
 
@@ -27,10 +27,11 @@ describe('animated_sprite', function ()
     it('should return a string describing data, current step and local frame', function ()
       local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = true
+      anim_spr.play_speed_frame = 1.5
       anim_spr.current_anim_key = "idle"
       anim_spr.current_step = 2
       anim_spr.local_frame = 5
-      assert.are_equal("animated_sprite({loop = animated_sprite_data([2 sprites], 10, true), no_loop = animated_sprite_data([2 sprites], 10, false)}, true, idle, 2, 5)", anim_spr:_tostring())
+      assert.are_equal("animated_sprite({loop = animated_sprite_data([3 sprites], 10, true), no_loop = animated_sprite_data([2 sprites], 10, false)}, true, 1.5, idle, 2, 5)", anim_spr:_tostring())
     end)
 
   end)
@@ -92,6 +93,30 @@ describe('animated_sprite', function ()
         {anim_spr.playing, anim_spr.current_anim_key, anim_spr.current_step, anim_spr.local_frame})
     end)
 
+    it('set play speed to 1 by default', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = false
+      anim_spr.current_anim_key = "no_loop"
+      anim_spr.current_step = 0
+      anim_spr.local_frame = 0
+
+      anim_spr:play("no_loop", false)
+
+      assert.are_equal(1, anim_spr.play_speed)
+    end)
+
+    it('set play speed to any custom speed', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = false
+      anim_spr.current_anim_key = "no_loop"
+      anim_spr.current_step = 0
+      anim_spr.local_frame = 0
+
+      anim_spr:play("no_loop", true, 2.3)
+
+      assert.are_equal(2.3, anim_spr.play_speed)
+    end)
+
   end)
 
   describe('update', function ()
@@ -99,6 +124,7 @@ describe('animated_sprite', function ()
     it('should do nothing when not playing', function ()
       local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = false
+      anim_spr.play_speed_frame = 1
       anim_spr.current_step = 9
       anim_spr.local_frame = 99
 
@@ -108,9 +134,10 @@ describe('animated_sprite', function ()
         {anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
     end)
 
-    it('should increment the local frame if under the animation step_frames', function ()
+    it('should increment the local frame if under the animation step_frames at playback speed 1', function ()
       local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = true
+      anim_spr.play_speed_frame = 1
       anim_spr.current_anim_key = "loop"
       anim_spr.current_step = 1
       anim_spr.local_frame = 8  -- data.step_frames is 10, so frames play from 0 to 9
@@ -121,9 +148,24 @@ describe('animated_sprite', function ()
         {anim_spr.current_step, anim_spr.local_frame})
     end)
 
-    it('should reset local frame and enter next step when step_frames is reached', function ()
+    it('should increase the local frame with playback speed if under the animation step_frames', function ()
       local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = true
+      anim_spr.play_speed_frame = 1.5
+      anim_spr.current_anim_key = "loop"
+      anim_spr.current_step = 1
+      anim_spr.local_frame = 8.2  -- data.step_frames is 10, so frames play from 0 to 9
+
+      anim_spr:update()
+
+      assert.are_same({1, 9.7},
+        {anim_spr.current_step, anim_spr.local_frame})
+    end)
+
+    it('should reset local frame and enter next step when step_frames is reached at playback speed 1', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = true
+      anim_spr.play_speed_frame = 1
       anim_spr.current_anim_key = "loop"
       anim_spr.current_step = 1
       anim_spr.local_frame = 9  -- data.step_frames - 1
@@ -134,11 +176,42 @@ describe('animated_sprite', function ()
         {anim_spr.current_step, anim_spr.local_frame})
     end)
 
+    it('should decrease the local frame by step_frames and enter next step when step_frames is reached when playback speed is not 1', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = true
+      anim_spr.play_speed_frame = 2
+      anim_spr.current_anim_key = "loop"
+      anim_spr.current_step = 1
+      anim_spr.local_frame = 9  -- data.step_frames - 1
+
+      anim_spr:update()
+
+      assert.are_same({2, 1},
+        {anim_spr.current_step, anim_spr.local_frame})
+    end)
+
+    it('should decrease the local frame by 2*step_frames and advance by 2 steps when playback speed is enough to cover 2 step_frames (with initial fraction offset)', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = true
+      anim_spr.play_speed_frame = 14.5
+      anim_spr.current_anim_key = "loop"
+      anim_spr.current_step = 1
+      anim_spr.local_frame = 8
+      -- data.step_frames = 10, and we will reach 8 + 14.5 = 22.5, so 2 steps ahead and 2.5 remaining
+      -- this is testing the internal loop supporting high playback speeds with remainders in chain
+
+      anim_spr:update()
+
+      assert.are_same({3, 2.5},
+        {anim_spr.current_step, anim_spr.local_frame})
+    end)
+
     it('should continue playing from the start when looping and end of animation has been reached', function ()
       local anim_spr = animated_sprite(anim_spr_data_table)
       anim_spr.playing = true
+      anim_spr.play_speed_frame = 1
       anim_spr.current_anim_key = "loop"
-      anim_spr.current_step = 2
+      anim_spr.current_step = 3
       anim_spr.local_frame = 9  -- data.step_frames - 1
 
       anim_spr:update()
@@ -147,16 +220,62 @@ describe('animated_sprite', function ()
         {anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
     end)
 
-    it('should stop playing when not looping and end of animation has been reached', function ()
+    it('should continue playing from the start when looping and end of animation has been reached, with any remaining frame fraction', function ()
       local anim_spr = animated_sprite(anim_spr_data_table)
-      anim_spr.current_anim_key = "no_loop"
       anim_spr.playing = true
-      anim_spr.current_step = 2
+      anim_spr.play_speed_frame = 2.5
+      anim_spr.current_anim_key = "loop"
+      anim_spr.current_step = 3
       anim_spr.local_frame = 9  -- data.step_frames - 1
 
       anim_spr:update()
 
-      assert.are_same({false, 2, 9},
+      assert.are_same({true, 1, 1.5},
+        {anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
+    end)
+
+    it('should continue playing from the start when looping and end of animation has been reached with a high playback speed skipping 1 frame', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = true
+      anim_spr.play_speed_frame = 17
+      anim_spr.current_anim_key = "loop"
+      anim_spr.current_step = 2
+      anim_spr.local_frame = 5
+      -- data.step_frames = 10, and we will reach 5 + 17 = 22, so 2 steps ahead and 2 remaining, but there are only 3 steps
+      -- so we go back to 1
+      -- this is testing the internal loop supporting high playback speeds with remainders in chain
+
+      anim_spr:update()
+
+      assert.are_same({true, 1, 2},
+        {anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
+    end)
+
+    it('should stop playing when not looping and end of animation has been reached, keeping local frame equal to step frames', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = true
+      anim_spr.play_speed_frame = 1
+      anim_spr.current_anim_key = "no_loop"
+      anim_spr.current_step = 3
+      anim_spr.local_frame = 9  -- data.step_frames - 1
+
+      anim_spr:update()
+
+      assert.are_same({false, 3, 10},  -- 10 doesn't exist, but ok for stopped anim
+        {anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
+    end)
+
+    it('should stop playing when not looping and end of animation has been reached, keeping even a local_frame beyond last frame', function ()
+      local anim_spr = animated_sprite(anim_spr_data_table)
+      anim_spr.playing = true
+      anim_spr.play_speed_frame = 1.5
+      anim_spr.current_anim_key = "no_loop"
+      anim_spr.current_step = 3
+      anim_spr.local_frame = 9  -- data.step_frames - 1
+
+      anim_spr:update()
+
+      assert.are_same({false, 3, 10.5},  -- 10.5 doesn't exist, but ok for stopped anim
         {anim_spr.playing, anim_spr.current_step, anim_spr.local_frame})
     end)
 

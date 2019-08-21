d6b63959c29539e8f1a573dbf2c04c4956bccfb7
hsandt <hs@fuwar.in>
2019-08-21 21:21:50 +0100

    [BUILD] Improved path helper to exclude legit dot usages like '...' or '..mydir..'

--- helper/path_helper.sh
+++ helper/path_helper.sh
@@ -1,4 +1,4 @@
 # make sure paths passed are not absolute or backward relative to avoid deletion of upper paths
 function is_unsafe_path {
-	[[ $1 == /* ]] || [[ $1 == *..* ]]
+  [[ $1 == /* ]] || [[ $1 == .. ]] || [[ $1 == ../* ]] || [[ $1 == */.. ]] || [[ $1 == */../* ]]
 }

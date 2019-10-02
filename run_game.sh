3aca28229f4a8ba24266c218182baa4d96385a39
hsandt <hs@fuwar.in>
2019-10-02 22:08:54 +0100

    [RUN] Fix cartridge name

--- run_game.sh
+++ run_game.sh
@@ -2,6 +2,11 @@
 
 # Run game.p8 with PICO-8 executable
 # Pass any extra arguments to pico8
-run_cmd="pico8 -run build/pico-sonic.p8 -screenshot_scale 4 -gif_scale 4 $@"
+
+# Configuration: cartridge
+cartridge_stem="picosonic"
+version="3.0"
+
+run_cmd="pico8 -run build/${cartridge_stem}_v${version}.p8 -screenshot_scale 4 -gif_scale 4 $@"
 echo "> $run_cmd"
 bash -c "$run_cmd"

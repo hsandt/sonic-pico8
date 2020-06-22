9aa76185c796f16da5b0e06207b4952fb4bc9a93
Hyper Sonic <hs@fuwar.in>
2020-06-22 17:57:03 +0200

    [TEST] Moved functions from itest_main to integrationtest

--- src/itest_main.lua
+++ src/itest_main.lua
@@ -4,28 +4,47 @@
 require("engine/pico8/api")
 
 require("engine/test/integrationtest")
-local picosonic_app = require("application/picosonic_app")
--- tag to add require for itest files here
---[[add_require]]
 
 --#if log
 local logging = require("engine/debug/logging")
 --#endif
 
-local current_itest_index = 0
+local picosonic_app = require("application/picosonic_app")
+
+-- set app immediately so during itest registration by require,
+--   time_trigger can access app fps
+local app = wit_fighter_app()
+itest_runner.app = app
+
+-- tag to add require for itest files here
+--[[add_require]]
 
 function _init()
 --#if log
   -- register log streams to output logs to both the console and the file log
   logging.logger:register_stream(logging.console_log_stream)
   logging.logger:register_stream(logging.file_log_stream)
+  logging.file_log_stream.file_prefix = "picosonic"
 
   -- clear log file on new itest session
   logging.file_log_stream:clear()
+
+  logging.logger.active_categories = {
+    -- engine
+    ['default'] = true,
+    -- ['codetuner'] = nil,
+    -- ['flow'] = nil,
+    ['itest'] = true,
+    -- ['log'] = nil,
+    -- ['ui'] = nil,
+    -- ['frame'] = nil,
+
+    -- game
+    -- ['...'] = true,
+  }
 --#endif
 
-  itest_runner.app = picosonic_app()
-  picosonic_app.initial_gamestate = ':main_menu'
+  picosonic_app.initial_gamestate = ':titlemenu'
 
   -- start first itest
   init_game_and_start_next_itest()
@@ -40,27 +59,13 @@ function _draw()
   itest_runner:draw_game_and_test()
 end
 
-function init_game_and_start_next_itest()
-  init_game_and_start_itest_by_relative_index(1)
-end
-
-function init_game_and_start_itest_by_relative_index(delta)
-  -- clamp new index
-  local new_index = mid(1, current_itest_index + delta, #itest_manager.itests)
-  -- check that an effective idnex change occurs
-  if new_index ~= current_itest_index then
-    current_itest_index = new_index
-    itest_manager:init_game_and_start_by_index(new_index)
-  end
-end
-
 -- press left/right to navigate freely in itests, even if not finished
 -- press x to skip itest only if finished
 function handle_input()
   -- since input.mode is simulated during itests, use pico8 api directly for input
   if btnp(button_ids.left) then
     -- go back to previous itest
-    init_game_and_start_itest_by_relative_index(-1)
+    itest_manager:init_game_and_start_itest_by_relative_index(-1)
     return
   elseif btnp(button_ids.right) then
     -- skip current itest
@@ -68,11 +73,11 @@ function handle_input()
     return
   elseif btnp(button_ids.up) then
     -- go back 10 itests
-    init_game_and_start_itest_by_relative_index(-10)
+    itest_manager:init_game_and_start_itest_by_relative_index(-10)
     return
   elseif btnp(button_ids.down) then
     -- skip many itests
-    init_game_and_start_itest_by_relative_index(10)
+    itest_manager:init_game_and_start_itest_by_relative_index(10)
     return
   end
 

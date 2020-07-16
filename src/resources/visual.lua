945d976a617b77624937adc208daf6a6fa6c6df2
Hyper Sonic <hs@gamedesignshortcut.com>
2020-07-16 16:17:36 +0200

    [VISUAL] Fixed cursor pink background in debug by adding transparent color

--- src/resources/visual.lua
+++ src/resources/visual.lua
@@ -1,10 +1,11 @@
+require("engine/render/color")
 local sprite_data = require("engine/render/sprite_data")
 
 local visual = {}
 
 local sprite_data_t = {
 --#if mouse
-  cursor = sprite_data(sprite_id_location(1, 0))
+  cursor = sprite_data(sprite_id_location(1, 0), nil, nil, colors.pink)
 --#endif
 }
 

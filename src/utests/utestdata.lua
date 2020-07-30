932bc259e5d065d626a6c5392ff98d1cb1d337e3
huulong <n.huu.long@gmail.com>
2020-07-30 23:21:09 +0200

    [TEST] Fixed pico8 utests - chmod +x build_pico8_utests.sh and run_pico8_utests.sh - require pico8/api and engine/common in utest_main - fixed data tests in utestdata to match new tiles (real and proto)

--- src/utests/utestdata.lua
+++ src/utests/utestdata.lua
@@ -11,33 +11,34 @@ local collision_data = require("data/collision_data")
 -- after minification if keys are not protected with ["key"] syntax)
 local playercharacter_data = require("data/playercharacter_data")
 
-check('sprite_id_location(0, 4) should have collision flag set', function ()
-  local sprite_id = sprite_id_location(0, 4):to_sprite_id()
-  assert(fget(sprite_id, sprite_flags.collision), "sprite_id_location(0, 4) has collision flag unset")
+check('sprite_id_location(1, 3) should have collision flag set', function (utest_name)
+  local sprite_id = sprite_id_location(1, 3):to_sprite_id()
+  assert(fget(sprite_id, sprite_flags.collision), "sprite_id_location(0, 4) has collision flag unset", utest_name)
 end)
 
-check('sprite_id_location(0, 4) should have collision mask id set to location below', function ()
-  local sprite_id = sprite_id_location(0, 4):to_sprite_id()
-  assert(collision_data.tiles_data[sprite_id] == tile_data(sprite_id_location(0, 5), 0))
+check('sprite_id_location(1, 3) should have collision mask id set to location above, angle atan2(8, 2)', function (utest_name)
+  local sprite_id = sprite_id_location(1, 3):to_sprite_id()
+  assert(collision_data.tiles_data[sprite_id] == tile_data(sprite_id_location(1, 2), atan2(8, 2)), utest_name)
 end)
 
-check('. height_array._fill_array on sprite_id_location(0, 5) should fill the array with tile mask data: full', function ()
+check('height_array._fill_array on sprite_id_location(2, 3) should fill the array with tile mask data: full', function (utest_name)
   local array = {}
-  height_array._fill_array(array, sprite_id_location(0, 5))
-  assert(are_same_with_message({8, 8, 8, 8, 8, 8, 8, 8}, array))
+  height_array._fill_array(array, sprite_id_location(2, 3))
+  assert(are_same_with_message({8, 8, 8, 8, 8, 8, 8, 8}, array), utest_name)
 end)
 
 -- bugfix history: after switching to pink transparency, all my tiles became square blocks
-check('= height_array._fill_array on sprite_id_location(0, 5) the array with tile mask data: descending slope 45', function ()
+-- warning: it's a proto tile, if you strip it from final build later, test another tile instead
+check('= height_array._fill_array on sprite_id_location(1, 7) the array with tile mask data: descending slope 45', function (utest_name)
   local array = {}
-  height_array._fill_array(array, sprite_id_location(1, 5))
-  assert(are_same_with_message({1, 2, 3, 4, 5, 6, 7, 8}, array))
+  height_array._fill_array(array, sprite_id_location(1, 7))
+  assert(are_same_with_message({1, 2, 3, 4, 5, 6, 7, 8}, array), utest_name)
 end)
 
-check('sonic_sprite_data_table preserved key "idle"', function ()
-  assert(playercharacter_data.sonic_sprite_data_table["idle"] ~= nil)
+check('sonic_sprite_data_table preserved key "idle"', function (utest_name)
+  assert(playercharacter_data.sonic_sprite_data_table["idle"] ~= nil, utest_name)
 end)
 
-check('sonic_animated_sprite_data_table preserved key "idle"', function ()
-  assert(playercharacter_data.sonic_animated_sprite_data_table["idle"] ~= nil)
+check('sonic_animated_sprite_data_table preserved key "idle"', function (utest_name)
+  assert(playercharacter_data.sonic_animated_sprite_data_table["idle"] ~= nil, utest_name)
 end)

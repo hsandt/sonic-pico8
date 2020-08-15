cba147fc509003e857a5f15cc5ba70c71bfb8653
huulong <n.huu.long@gmail.com>
2020-08-15 22:55:44 +0200

    [TILE] Generalized tile collision data processing to all interior directions

--- src/data/tile_collision_data.lua
+++ src/data/tile_collision_data.lua
@@ -1,5 +1,10 @@
 local tile = {}
 
+-- struct that contains directly usable data on tile collision
+-- semantically, it is almost equivalent to raw_tile_collision_data
+--  (technically it loses the mask sprite id location) and redundant,
+--  but by precomputing more detailed data using PICO-8 spritesheet / busted mocks,
+--  tile_collision_data makes it possible to check for collision with ground very easily
 local tile_collision_data = new_struct()
 
 -- height_array  [int]    sequence of column heights of a tile collision mask per column index,
@@ -18,10 +23,16 @@ local tile_collision_data = new_struct()
 --                         0.25 to 0.5:  horizontal interior right, vertical interior up   (ceiling right corner or flat)
 --                         0.5  to 0.75: horizontal interior left,  vertical interior up   (ceiling flat or left corner)
 --                         0.75 to 1:    horizontal interior left,  vertical interior down (desc slope or flat)
-function tile_collision_data:_init(height_array, width_array, slope_angle)
+-- interior_v    vertical_dirs     vertical direction of the tile's interior
+--                                 (up for ceiling, down for floor)
+-- interior_h    horizontal_dirs   horizontal direction of the tile's interior
+--                                 (left for desc slope or left ceiling, asc slope or right ceiling)
+function tile_collision_data:_init(height_array, width_array, slope_angle, interior_v, interior_h)
   self.height_array = height_array
   self.width_array = width_array
   self.slope_angle = slope_angle
+  self.interior_v = interior_v
+  self.interior_h = interior_h
 end
 
 -- return the height for a column index starting at 0, from left to right
@@ -35,43 +46,141 @@ function tile_collision_data:get_width(row_index0)
 end
 
 function tile_collision_data.from_raw_tile_collision_data(raw_data)
+  assert(raw_data.slope_angle >= 0 and raw_data.slope_angle < 1, "tile_collision_data.from_raw_tile_collision_data: raw_data.slope_angle is "..raw_data.slope_angle..", apply `% 1` before passing")
+  -- we don't mind edge cases (slope angle at 0, 0.25, 0.5 or 0.75 exactly)
+  --  and assume the code will handle any arbitrary decision on interior_h/v
+  local interior_v = raw_data.slope_angle < 0.5 and horizontal_dirs.right or horizontal_dirs.left
+  local interior_h = (raw_data.slope_angle < 0.25 or raw_data.slope_angle > 0.75) and vertical_dirs.down or vertical_dirs.up
+
   return tile_collision_data(
-    tile_collision_data.read_height_array(raw_data.id_loc, slope_angle),
-    tile_collision_data.read_width_array(raw_data.id_loc, slope_angle),
-    raw_data.slope_angle
+    tile_collision_data.read_height_array(raw_data.mask_tile_id_loc, interior_v),
+    tile_collision_data.read_width_array(raw_data.mask_tile_id_loc, interior_h),
+    raw_data.slope_angle,
+    interior_v,
+    interior_h
   )
 end
 
--- Read tile mask located at tile_mask_id_location: sprite_id_location
+-- return a stateful range iterator
+-- this function is generic enough to be in picoboots helper
+--  but right now it's only used in this script so it is local
+-- normally we should make it accessible from other modules
+--  and test it, but it's likely to be correct since it was copied from
+-- http://lua-users.org/wiki/RangeIterator and this module's tests are passing
+local function range(from, to, step)
+  step = step or 1
+  return function(_, lastvalue)
+    local nextvalue = lastvalue + step
+    if step > 0 and nextvalue <= to or step < 0 and nextvalue >= to or
+       step == 0
+    then
+      return nextvalue
+    end
+  end, nil, from - step  -- from - step: trick to start at from on the first step
+end
+
+-- check for collision pixel at tile_mask_x + dx, tile_mask_y + dy,
+--  and if one is found, return the length of the mask strip segment (height or width that constitutes
+--  a collision array0) in the direction we were iterating on in calling context, thanks to segment_length_evaluator
+--  (it is needed because height is read on y, width on x, and depending on the interior's
+--  direction, we iterated from a different side and measure the length differently)
+-- if no collision pixel at this position, return nil
+function tile_collision_data.check_collision_pixel(tile_mask_x, tile_mask_y, dx, dy, interior_v, interior_h, segment_length_evaluator)
+  local tile_mask_color = sget(tile_mask_x + dx, tile_mask_y + dy)
+  -- we use black (0) as transparent mask color
+  if tile_mask_color ~= 0 then
+    -- segment_length_evaluator will either use dx & interior_v or dy & interior_h,
+    --  but we don't know from here so pass both
+    return segment_length_evaluator(dx, dy, interior_v, interior_h)
+  end
+end
+
+-- return height to fill collision height array when finding first collision pixel at dy,
+--  vertical interior on interior_v side
+function tile_collision_data.evaluate_collision_height(dx, dy, interior_v, interior_h)
+  if interior_v == vertical_dirs.down then
+    -- we were iterating from the sky to the floor
+    --  so the height is complementary to our iteration distance
+    -- if we hit a collision pixel on first iteration (dy == 0),
+    --  then the column is full (return tile_size), so no offset
+    return tile_size - dy
+  else
+    -- we were iterating from the bottom to the ceiling, but backward,
+    --  so our iteration index tells us how far from the top we are
+    -- if we hit a collision pixel on first iteration (dy == tile_size - 1)
+    --  then the column is full (return tile_size), so need offset +1
+    return dy + 1
+  end
+end
+
+-- return height to fill collision width array when finding first collision pixel at dx,
+--  horizontal interior on interior_h side
+function tile_collision_data.evaluate_collision_width(dx, dy, interior_v, interior_h)
+  -- see comments in evaluate_collision_height and transpose everything
+  if interior_h == horizontal_dirs.right then
+    return tile_size - dx
+  else
+    return dx + 1
+  end
+end
+
+-- Read tile mask collision height array located at tile_mask_id_location: sprite_id_location
 -- We assume that the tile mask is only compounded or white (more exactly non-black)
 --  vertical segments aka "columns" touching the top (if interior is up) or bottom (if interior is down)
 --  of the tile.
-function tile_collision_data.read_height_array(tile_mask_id_location, slope_angle)
+function tile_collision_data.read_height_array(tile_mask_id_location, interior_v)
   local array = {}
-  -- todo: support ceiling
+
   local tile_mask_topleft_position = tile_mask_id_location:to_topleft_position()
-  -- iterate over columns from left to right, searching for the highest filled pixel
+
+  -- range returns a tuple so we need to pack and unpack later
+  local y_range = interior_v == vertical_dirs.down and {range(0, tile_size - 1)} or {range(tile_size - 1, 0, -1)}
+
+  -- iterate over columns from left to right (order doesn't matter as long as we fill the height array in the same order)
   for dx = 0, tile_size - 1 do
-    -- iterate from the top of the column and stop at the first filled pixel (we assume
-    -- lower pixels are also filled for readability of the tile mask, but not enforced)
-    local mask_height = 0
-    for dy = 0, tile_size - 1 do
-      local tile_mask_color = sget(tile_mask_topleft_position.x + dx, tile_mask_topleft_position.y + dy)
-      -- we use black (0) as transparent mask color
-      if tile_mask_color ~= 0 then
-        mask_height = tile_size - dy
+    -- iterate from the opposite side of the vertical interior (e.g. bottom if interior is ceiling)
+    --  so we can find the collision pixel the farthest from the interior, which really represents the column height
+    for dy in unpack(y_range) do
+      -- no need to pass interior_h (but we need dx to check pixel at this position)
+      column_height = tile_collision_data.check_collision_pixel(tile_mask_topleft_position.x, tile_mask_topleft_position.y, dx, dy, interior_v, nil, tile_collision_data.evaluate_collision_height)
+      if column_height then
+        -- collision pixel found at column_height
+        -- break so we can immediately store the column_height in the array
         break
       end
     end
-    add(array, mask_height)
+    if not column_height then
+      -- no pixel found at all, so column width is 0
+      column_height = 0
+    end
+    add(array, column_height)
   end
   return array
 end
 
-function tile_collision_data.read_width_array(tile_mask_id_location, slope_angle)
-  -- todo: probably make a unique function read_array and pass some parameter x/y
-  -- to distinguish height and width parsing
-  return {}
+-- see read_height_array and transpose everything for comments:
+-- height -> width
+-- x <-> y
+-- interior_v -> interior_h
+-- up -> left and down -> right
+function tile_collision_data.read_width_array(tile_mask_id_location, interior_h)
+  local array = {}
+  local tile_mask_topleft_position = tile_mask_id_location:to_topleft_position()
+  local x_range = interior_h == horizontal_dirs.right and {range(0, tile_size - 1)} or {range(tile_size - 1, 0, -1)}
+
+  for dy = 0, tile_size - 1 do
+    for dx in unpack(x_range) do
+      row_width = tile_collision_data.check_collision_pixel(tile_mask_topleft_position.x, tile_mask_topleft_position.y, dx, dy, nil, interior_h, tile_collision_data.evaluate_collision_width)
+      if row_width then
+        break
+      end
+    end
+    if not row_width then
+      row_width = 0
+    end
+    add(array, row_width)
+  end
+  return array
 end
 
 return tile_collision_data
--- src/data/tile_collision_data_utest.lua
+++ src/data/tile_collision_data_utest.lua
@@ -1,69 +1,229 @@
 require("engine/test/bustedhelper")
 local tile_collision_data = require("data/tile_collision_data")
 
+local raw_tile_collision_data = require("data/raw_tile_collision_data")
+
+-- when we have to mock tile sprite data in PICO-8,
+-- we use the following
+
+-- mask tile 1: bottom-right asc slope
+-- pixel representation:
+-- ........
+-- ........
+-- ........
+-- ........
+-- ......##
+-- ....####
+-- ..######
+-- ########
+
+-- mask tile 2: top-left concave ceiling
+-- pixel representation:
+-- ########
+-- ######..
+-- ####....
+-- ###.....
+-- ##......
+-- ##......
+-- #.......
+-- #.......
+
 describe('tile_collision_data', function ()
 
-  describe("mocking _fill_array", function ()
+  describe('_init', function ()
 
-    describe('_init', function ()
-
-      it('should create a tile_collision_data with reciprocal arrays and slope angle', function ()
-        local tcd = tile_collision_data({1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, 0.125)
-        assert.are_same({{1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, 0.125}, {tcd.height_array, tcd.width_array, tcd.slope_angle})
-      end)
-
-    end)
-
-    describe('get_height', function ()
-
-      it('should return the height at the given column index', function ()
-        local tcd = tile_collision_data({1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, 0.125)
-        assert.are_equal(2, tcd:get_height(2))
-      end)
-
-    end)
-
-    describe('get_width', function ()
-
-      it('should return the width at the given column index', function ()
-        local tcd = tile_collision_data({1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, 0.125)
-        assert.are_equal(2, tcd:get_width(4))
-      end)
-
+    it('should create a tile_collision_data with reciprocal arrays and slope angle', function ()
+      local tcd = tile_collision_data({1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, atan2(8, -4), horizontal_dirs.right, vertical_dirs.down)
+      assert.are_same({{1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, atan2(8, -4)}, {tcd.height_array, tcd.width_array, tcd.slope_angle})
     end)
 
   end)
 
-  describe('read_height_array', function ()
+  describe('get_height', function ()
 
-    local sget_mock
+    it('should return the height at the given column index', function ()
+      local tcd = tile_collision_data({1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, atan2(8, -4))
+      assert.are_equal(2, tcd:get_height(2))
+    end)
+
+  end)
+
+  describe('get_width', function ()
+
+    it('should return the width at the given column index', function ()
+      local tcd = tile_collision_data({1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, atan2(8, -4))
+      assert.are_equal(2, tcd:get_width(4))
+    end)
+
+  end)
+
+  describe('from_raw_tile_collision_data', function ()
 
     setup(function ()
-      -- simulate an sget that would return the pixel of a tile mask
-      --  if coordinates fall in the sprite at location (1, 2), i.e. [8-15] x [16-23],
-      --  where mock_height_array contains the respective height of the mask columns
-      --  for each column from left to right
-      local mock_height_array = {2, 3, 5, 6, 0, 1, 4, 2}
-      sget_mock = stub(_G, "sget", function (x, y)
-        if x >= 8 and x <= 15 and y >= 16 and y <= 23 then
-          -- return filled pixel color iff below mask height on this column
-          local height = mock_height_array[x - 7]
-          if y - 16 >= tile_size - height then
-            return 1
-          else
-            return 0
-          end
+      stub(tile_collision_data, "read_height_array", function (tile_mask_id_location, slope_angle)
+        printh("tile_mask_id_location: "..dump(tile_mask_id_location))
+        if tile_mask_id_location == 1 then
+          return {1, 1, 2, 2, 3, 3, 4, 4}
+        else
+          return {8, 6, 4, 3, 2, 2, 1, 1}
+        end
+      end)
+      stub(tile_collision_data, "read_width_array", function (tile_mask_id_location, slope_angle)
+        if tile_mask_id_location == 1 then
+          return {0, 0, 0, 0, 2, 4, 6, 8}
+        else
+          return {8, 6, 4, 3, 2, 2, 1, 1}
         end
       end)
     end)
 
     teardown(function ()
-      sget_mock:revert()
+      tile_collision_data.read_height_array:revert()
+      tile_collision_data.read_width_array:revert()
     end)
 
-    it('should fill the array with ', function ()
-      local array = tile_collision_data.read_height_array(sprite_id_location(1, 2), 0)
-      assert.are_same({2, 3, 5, 6, 0, 1, 4, 2}, array)
+    it('should return a tile_collision_data containing (mock tile 1) height/width array, slope angle, derived interior directions', function ()
+      local raw_data = raw_tile_collision_data(1, atan2(8, -4))
+      local tcd = tile_collision_data.from_raw_tile_collision_data(raw_data)
+      -- struct equality with are_equal would work, we just use are_same to benefit from diff asterisk provided by luassert
+      assert.are_same(tile_collision_data({1, 1, 2, 2, 3, 3, 4, 4}, {0, 0, 0, 0, 2, 4, 6, 8}, atan2(8, -4), horizontal_dirs.right, vertical_dirs.down), tcd)
+    end)
+
+    it('should return a tile_collision_data containing (mock tile 2) height/width array, slope angle, derived interior directions', function ()
+      local raw_data = raw_tile_collision_data(2, atan2(-8, 8))
+      local tcd = tile_collision_data.from_raw_tile_collision_data(raw_data)
+      -- struct equality with are_equal would work, we just use are_same to benefit from diff asterisk provided by luassert
+      assert.are_same(tile_collision_data({8, 6, 4, 3, 2, 2, 1, 1}, {8, 6, 4, 3, 2, 2, 1, 1}, atan2(-8, 8), horizontal_dirs.left, vertical_dirs.up), tcd)
+    end)
+
+  end)
+
+  describe('(mock sget)', function ()
+
+    local sget_mock
+
+    setup(function ()
+      local mock_mask_dot_matrix1 = {
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1},
+        {0, 0, 0, 0, 1, 1, 1, 1},
+        {0, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+      }
+
+      local mock_mask_dot_matrix2 = {
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 0},
+        {1, 1, 1, 1, 0, 0, 0, 0},
+        {1, 1, 1, 0, 0, 0, 0, 0},
+        {1, 1, 0, 0, 0, 0, 0, 0},
+        {1, 1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0},
+      }
+
+      -- simulate an sget that would return the pixel of a tile mask
+      --  if coordinates fall in the sprite 1 at location (1, 0), i.e. [8-15] x [0-8],
+      --  or sprite 2 at location (2, 0), i.e. [16-23] x [0-8]
+      stub(_G, "sget", function (x, y)
+        if x >= 8 and x <= 15 and y >= 0 and y <= 8 then
+          -- convert offset to 1-based Lua index
+          -- multi-dimensional array above is first indexed by row (j), then column (i)
+          return mock_mask_dot_matrix1[y+1][x-8+1]
+        elseif x >= 16 and x <= 23 and y >= 0 and y <= 8 then
+          return mock_mask_dot_matrix2[y+1][x-16+1]
+        end
+        return 0
+      end)
+    end)
+
+    teardown(function ()
+      sget:revert()
+    end)
+
+    -- read_height_array utests could be done without mocking sget
+    --  and mocking check_collision_pixel instead, but since we had already written the utests below
+    -- (which check the final result without stubbing) before extracting check_collision_pixel,
+    -- it was simpler to just keep them, that to create a stub for check_collision_pixel that would cheat a lot
+    -- with the passed arguments
+    describe('read_height_array', function ()
+
+      it('should return an array with respective column heights, from left to right', function ()
+        local array = tile_collision_data.read_height_array(sprite_id_location(1, 0), vertical_dirs.down)
+        assert.are_same({1, 1, 2, 2, 3, 3, 4, 4}, array)
+      end)
+
+      it('should return an array with respective column heights, from left to right', function ()
+        local array = tile_collision_data.read_height_array(sprite_id_location(2, 0), vertical_dirs.up)
+        assert.are_same({8, 6, 4, 3, 2, 2, 1, 1}, array)
+      end)
+
+      it('should return an array with respective column rows, from top to bottom', function ()
+        local array = tile_collision_data.read_width_array(sprite_id_location(1, 0), horizontal_dirs.right)
+        assert.are_same({0, 0, 0, 0, 2, 4, 6, 8}, array)
+      end)
+
+      it('should return an array with respective column rows, from top to bottom', function ()
+        local array = tile_collision_data.read_width_array(sprite_id_location(2, 0), horizontal_dirs.left)
+        assert.are_same({8, 6, 4, 3, 2, 2, 1, 1}, array)
+      end)
+
+    end)
+
+    describe('check_collision_pixel', function ()
+
+      it('(mock tile 1) should return nil when column pixel falls on empty pixel (interior down)', function ()
+        -- note that 5 from top means 6th pixel on this column from the top
+        local column_height = tile_collision_data.check_collision_pixel(8, 0, 2, 5, vertical_dirs.down, nil, tile_collision_data.evaluate_collision_height)
+        assert.are_equal(nil, column_height)
+      end)
+
+      it('(mock tile 1) should return 2 when column pixel falls on collision pixel at height 2 from bottom (interior down)', function ()
+        local column_height = tile_collision_data.check_collision_pixel(8, 0, 2, 6, vertical_dirs.down, nil, tile_collision_data.evaluate_collision_height)
+        assert.are_equal(2, column_height)
+      end)
+
+      it('(mock tile 2) should return nil when column pixel falls on empty pixel (interior up)', function ()
+        local column_height = tile_collision_data.check_collision_pixel(16, 0, 2, 4, vertical_dirs.up, nil, tile_collision_data.evaluate_collision_height)
+        assert.are_equal(nil, column_height)
+      end)
+
+      it('(mock tile 2) should return 4 when column pixel falls on collision pixel at height 2 from top (interior up)', function ()
+        local column_height = tile_collision_data.check_collision_pixel(16, 0, 2, 3, vertical_dirs.up, nil, tile_collision_data.evaluate_collision_height)
+        assert.are_equal(4, column_height)
+      end)
+
+    end)
+
+  end)  -- stub sget
+
+  describe('evaluate_collision_height', function ()
+
+    it('return tile_size - dy for interior down', function ()
+      local column_height = tile_collision_data.evaluate_collision_height(nil, 2, vertical_dirs.down, nil)
+      assert.are_equal(6, column_height)
+    end)
+
+    it('return dy + 1 for interior up', function ()
+      local column_height = tile_collision_data.evaluate_collision_height(nil, 3, vertical_dirs.up, nil)
+      assert.are_equal(4, column_height)
+    end)
+
+  end)
+
+  describe('evaluate_collision_width', function ()
+
+    it('return tile_size - dx for interior down', function ()
+      local row_width = tile_collision_data.evaluate_collision_width(2, nil, nil, horizontal_dirs.right)
+      assert.are_equal(6, row_width)
+    end)
+
+    it('return dx + 1 for interior up', function ()
+      local row_width = tile_collision_data.evaluate_collision_width(3, nil, nil, horizontal_dirs.left)
+      assert.are_equal(4, row_width)
     end)
 
   end)

8ee1ca3da73494c2d66e567459bde911ceb4712e
huulong <n.huu.long@gmail.com>
2020-08-21 18:08:04 +0200

    [RUN] Added -gif_len 60 to capture up to 1mn of GIF in PICO-8

--- run_game.sh
+++ run_game.sh
@@ -8,7 +8,7 @@ cartridge_stem="picosonic"
 version="3.1"
 config="$1"; shift
 
-run_cmd="pico8 -run build/${cartridge_stem}_v${version}_${config}.p8 -screenshot_scale 4 -gif_scale 4 $@"
+run_cmd="pico8 -run build/${cartridge_stem}_v${version}_${config}.p8 -screenshot_scale 4 -gif_scale 4 -gif_len 60 $@"
 
 # Support UNIX platforms without gnome-terminal by checking if the command exists
 # If you `reload.sh` the game, the separate terminal allows you to keep watching the program output,

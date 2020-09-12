267c5218eeba3b5c7ee8c4cd3e208e8441889048
huulong <n.huu.long@gmail.com>
2020-09-13 01:47:13 +0200

    [EMERALD] Draw emerald with color palette change based on number

--- src/ingame/emerald.lua
+++ src/ingame/emerald.lua
@@ -19,9 +19,16 @@ end
 
 -- render the emerald at its current location
 function emerald:render()
+  -- recolor emerald based on number
+  -- note that the reference emerald in the Red emerald,
+  --  which uses white (invariant), red (light color) and dark_purple (dark color)
+  --  so we must replace the last two with our custom colors
+  local custom_colors = visual.emerald_colors[self.number]
+  pal(colors.red, custom_colors[1])
+  pal(colors.dark_purple, custom_colors[2])
   -- pass center of tile, so emerald is represented with pivot at the center
-  printh("self.location:to_center_position(): "..dump(self.location:to_center_position()))
   visual.sprite_data_t.emerald:render(self.location:to_center_position())
+  pal()
 end
 
 return emerald
--- src/ingame/emerald_utest.lua
+++ src/ingame/emerald_utest.lua
@@ -29,14 +29,17 @@ describe('emerald', function ()
 
     setup(function ()
       stub(sprite_data, "render")
+      stub(_G, "pal")
     end)
 
     teardown(function ()
       sprite_data.render:revert()
+      pal:revert()
     end)
 
     after_each(function ()
       sprite_data.render:clear()
+      pal:clear()
     end)
 
     it('should draw emerald sprite data at tile center', function ()
@@ -46,6 +49,19 @@ describe('emerald', function ()
 
       assert.spy(sprite_data.render).was_called(1)
       assert.spy(sprite_data.render).was_called_with(match.ref(visual.sprite_data_t.emerald), vector(20, 12))
+    end)
+
+    it('should call pal with emerald colors matcing number, then clear palette change', function ()
+      local em = emerald(3, location(2, 1))
+
+      em:render()
+
+      -- unfortunately we cannot really test call order between pal and render,
+      --  so at least we check the call arguments
+      assert.spy(pal).was_called(3)
+      assert.spy(pal).was_called_with(colors.red, visual.emerald_colors[3][1])
+      assert.spy(pal).was_called_with(colors.dark_purple, visual.emerald_colors[3][2])
+      assert.spy(pal).was_called_with()
     end)
 
   end)

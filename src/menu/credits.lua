516e1e15ce8d9a341d8731e0a01fb6f048e3d5e6
huulong <n.huu.long@gmail.com>
2020-09-17 02:06:39 +0200

    [CREDITS] Added "credits" in credits screen to be clear

--- src/menu/credits.lua
+++ src/menu/credits.lua
@@ -49,7 +49,7 @@ function credits:draw_credits_text()
   -- top
   local y = 2
 
-  ui.print_aligned("pico-sonic", 64, y, alignments.horizontal_center, text_color)
+  ui.print_aligned("pico-sonic - credits", 64, y, alignments.horizontal_center, text_color)
   y = y + line_dy + paragraph_margin + 2
 
   api.print("sonic team", margin_x, y, text_color)

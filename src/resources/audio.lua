6ad6b3036d6fd233c7729f961f68d933e762eee4
huulong <n.huu.long@gmail.com>
2020-10-15 00:21:59 +0200

    [TITLE] Defined menu_select SFX (already plugged in menu_with_sfx)

--- src/resources/audio.lua
+++ src/resources/audio.lua
@@ -1,14 +1,17 @@
 local audio = {}
 
 local sfx_ids = {
+  -- builtin_data_titlemenu only
+  menu_select = 50,
+  -- menu_confirm = ??,
+
+  -- builtin_data_ingame only
   pick_emerald = 57,
   goal_reached = 58,
   jump = 59,
   spring_jump = 60,
   roll = 61,
   brake = 62,
-  -- menu_select = ??,
-  -- menu_confirm = ??,
 }
 
 local jingle_ids = {

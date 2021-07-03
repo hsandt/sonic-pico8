ad77ef4133155bb0d9e2e48d54131c382ffd5233
huulong <n.huu.long@gmail.com>
2021-07-03 17:31:52 +0200

    [TEST] Fixed picosonic app ingame utest now that we use local functions for menuitem and cannot stub/spy them

--- src/application/picosonic_app_ingame_utest.lua
+++ src/application/picosonic_app_ingame_utest.lua
@@ -25,19 +25,16 @@ describe('picosonic_app_ingame', function ()
 
     setup(function ()
       stub(picosonic_app_base, "on_post_start")
-      stub(picosonic_app_ingame, "create_late_jump_delay_menuitem")
       stub(_G, "menuitem")
     end)
 
     teardown(function ()
       picosonic_app_base.on_post_start:revert()
-      picosonic_app_ingame.create_late_jump_delay_menuitem:revert()
       menuitem:revert()
     end)
 
     after_each(function ()
       picosonic_app_base.on_post_start:clear()
-      picosonic_app_ingame.create_late_jump_delay_menuitem:clear()
       menuitem:clear()
     end)
 
@@ -52,16 +49,9 @@ describe('picosonic_app_ingame', function ()
       assert.are_equal(pc_data.max_late_jump_max_delay, app.get_late_jump_max_delay())
     end)
 
-    it('should call create_late_jump_delay_menuitem', function ()
+    it('should create 4 menu items', function ()
       app:on_post_start()
-      assert.spy(picosonic_app_ingame.create_late_jump_delay_menuitem).was_called(1)
-      assert.spy(picosonic_app_ingame.create_late_jump_delay_menuitem).was_called_with(match.ref(app))
-    end)
-
-    it('should create 3 menu items', function ()
-      app:on_post_start()
-      -- note that we create 4, but create_late_jump_delay_menuitem is stubbed so we don't catch that 4th call
-      assert.spy(menuitem).was_called(3)
+      assert.spy(menuitem).was_called(4)
       -- no reference to lambda passed to menuitem, so don't test was_called_with
     end)
 

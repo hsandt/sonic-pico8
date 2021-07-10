b8f47bed24f2e4d371f9e7823792f03df8800c2f
huulong <n.huu.long@gmail.com>
2021-07-10 18:23:13 +0200

    [ATTRACT MODE] Exit attract mode by pressing O or X with fade-out

--- src/application/picosonic_app_attract_mode.lua
+++ src/application/picosonic_app_attract_mode.lua
@@ -6,6 +6,9 @@
 
 local picosonic_app_base = require("application/picosonic_app_base")
 
+local input = require("engine/input/input")
+local postprocess = require("engine/render/postprocess")
+
 local stage_state = require("ingame/stage_state")
 
 local picosonic_app_attract_mode = derived_class(picosonic_app_base)
@@ -17,10 +20,48 @@ end
 function picosonic_app_attract_mode:on_post_start() -- override (optional)
   picosonic_app_base.on_post_start(self)
 
+  -- postprocess for fade-out
+  -- usually this is done in gamestate, but attract mode uses the same stage_state
+  --  as ingame, so we'd need #if attract_mode to distinguish behavior, and it's simpler to
+  --  put dedicated behavior here
+  self.postproc = postprocess()
+  -- self.is_exiting = false  -- commented out to spare characters, as nil is equivalent
+
   menuitem(5, "back to title", function()
     -- prefer passing basename for compatibility with .p8.png
     load('picosonic_titlemenu')
   end)
+end
+
+function picosonic_app_attract_mode:on_update() -- override (optional)
+  -- handle input: press any button to leave attract mode (avoids entering pause menu)
+  -- usually inputs are handled in gamestate, but for same reason as self.postproc we do it here
+
+  if input:is_just_pressed(button_ids.o) or input:is_just_pressed(button_ids.x) then
+    self:exit_attract_mode()
+  end
+end
+
+function picosonic_app_attract_mode:on_render() -- override (render)
+  self.postproc:apply()
+end
+
+function picosonic_app_attract_mode:exit_attract_mode()
+  if not self.is_exiting then
+    self.is_exiting = true
+    self:start_coroutine(self.exit_attract_mode_async, self)
+  end
+end
+
+function picosonic_app_attract_mode:exit_attract_mode_async()
+  -- fade out
+  for i = 0, 5 do
+    self.postproc.darkness = i
+    yield_delay_frames(6)
+  end
+
+  -- prefer passing basename for compatibility with .p8.png
+  load('picosonic_titlemenu')
 end
 
 return picosonic_app_attract_mode

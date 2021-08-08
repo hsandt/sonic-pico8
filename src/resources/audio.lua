c1848e07b61507ffcd2f7eab2798bac257da72b3
huulong <n.huu.long@gmail.com>
2021-08-08 19:53:37 +0200

    [CHARS] -> 16225 (103%) Replace resources/audio constants too using new sub-table constant parsing

--- src/resources/audio.lua
+++ src/resources/audio.lua
@@ -1,3 +1,7 @@
+--#if game_constants
+--(when using replace_strings with --game-constant-module-path [this_data.lua], all namespaced constants
+-- below are replaced with their values (as strings), so this file can be skipped)
+
 local audio = {}
 
 audio.sfx_ids = {
@@ -31,5 +35,8 @@ audio.music_ids = {
   -- builtin_data_titlemenu only (overlaps stage bgm in data_bgm1.p8)
   title = 0,
 }
+
+--(game_constants)
+--#endif
 
 return audio

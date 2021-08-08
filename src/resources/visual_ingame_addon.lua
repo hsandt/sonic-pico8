e16be3c617973116eafbb32a0f3d4435859981af
huulong <n.huu.long@gmail.com>
2021-08-08 18:36:09 +0200

    [CHARS] -> 16347 (104%) Define ingame_sprite_data_t as raw tables + transform

--- src/resources/visual_ingame_addon.lua
+++ src/resources/visual_ingame_addon.lua
@@ -32,38 +32,45 @@ local ingame_visual = {
   -- emerald_repr_sprite_id will be derived from sprite data, see below
 }
 
--- TODO OPTIMIZE CHARS: a transform would probably reduce chars count
-local ingame_sprite_data_t = {
-  -- palm tree extension sprites
-  -- top pivot is located at top-left of core
-  palm_tree_leaves_top = sprite_data(sprite_id_location(12, 12), tile_vector(1, 2), vector(0, 16), colors.pink),
-  -- right side pivot is located at top-right of core
-  -- left side is a mirror of right side, and must be placed just on the left of the core
-  -- no multicolor transparency right now, but if you add sprites on the top-left of the palm tree leaves for compactness on the spritesheet,
-  --  you can, but they must have no common colors with the leaves, and you must add all of their unique colors to the transparency list {color1, color2, ...}
-  palm_tree_leaves_right = sprite_data(sprite_id_location(13, 12), tile_vector(3, 4), vector(0, 16), colors.pink),
+local ingame_sprite_data_t = transform(
+  {
+    -- sprite_data(id_loc: sprite_id_location([1], [2]), span: tile_vector([3], [4]), pivot: vector([5], [6]), transparent_color_arg: [7]),
+    -- parameters:                        {id_loc(2), span(2), pivot(2), transparent_color}
 
-  -- RUNTIME SPRITES (stage-specific and common runtime)
-  -- below need runtime sprites to be reloaded, overwriting collision masks
-  background_forest_bottom_hole = sprite_data(sprite_id_location(1, 0), tile_vector(2, 3), vector(0, 0), colors.pink),
-  background_forest_bottom_lightshaft = sprite_data(sprite_id_location(13, 1), tile_vector(3, 2), vector(0, 0), colors.pink),
-  emerald_silhouette = sprite_data(sprite_id_location(9, 1), nil, vector(3, 2), colors.pink),
-  goal_plate_goal = sprite_data(sprite_id_location(3, 0), tile_vector(3, 2), vector(12, 16), colors.pink),
-  goal_plate_sonic = sprite_data(sprite_id_location(6, 0), tile_vector(3, 2), vector(12, 16), colors.pink),
-  goal_plate_rotating_90 = sprite_data(sprite_id_location(0, 1), tile_vector(1, 2), vector(4, 16), colors.pink),
+    -- palm tree extension sprites
+    -- top pivot is located at top-left of core
+    palm_tree_leaves_top                = {   12, 12,    1, 2,    0, 16,       colors.pink},
+    -- right side pivot is located at top-right of core
+    -- left side is a mirror of right side, and must be placed just on the left of the core
+    -- no multicolor transparency right now, but if you add sprites on the top-left of the palm tree leaves for compactness on the spritesheet,
+    --  you can, but they must have no common colors with the leaves, and you must add all of their unique colors to the transparency list {color1, color2, ...}
+    palm_tree_leaves_right              = {   13, 12,    3, 4,    0, 16,       colors.pink},
 
-  -- rotating goal plates at 45 degrees are exceptions and placed in the common area despite only being used at runtime
-  --  this is simply because there was no space left for sprites 2-tile high in the runtime area; hence the high location j
-  goal_plate_rotating_45_ccw = sprite_data(sprite_id_location(6, 14), tile_vector(2, 2), vector(7, 16), colors.pink),
-  goal_plate_rotating_45_cw = sprite_data(sprite_id_location(8, 14), tile_vector(2, 2), vector(8, 16), colors.pink),
+    -- RUNTIME SPRITES (stage-specific and common runtime)
+    -- below need runtime sprites to be reloaded, overwriting collision masks
+    background_forest_bottom_hole       = {     1, 0,    2, 3,    0,  0,       colors.pink},
+    background_forest_bottom_lightshaft = {    13, 1,    3, 2,    0,  0,       colors.pink},
+    emerald_silhouette                  = {     9, 1,    1, 1,    3,  2,       colors.pink},
+    goal_plate_goal                     = {     3, 0,    3, 2,   12, 16,       colors.pink},
+    goal_plate_sonic                    = {     6, 0,    3, 2,   12, 16,       colors.pink},
+    goal_plate_rotating_90              = {     0, 1,    1, 2,    4, 16,       colors.pink},
 
-  -- emerald representation tile (left part) and object sprite (both parts)
-  emerald = sprite_data(sprite_id_location(10, 15), nil, vector(3, 2), colors.pink),
+    -- rotating goal plates at 45 degrees are exceptions and placed in the common area despite only being used at runtime
+    --  this is simply because there was no space left for sprites 2-tile high in the runtime area; hence the high location j
+    goal_plate_rotating_45_ccw          = {    6, 14,    2, 2,    7, 16,       colors.pink},
+    goal_plate_rotating_45_cw           = {    8, 14,    2, 2,    8, 16,       colors.pink},
 
-  -- spring (pivot at bottom center on both sprites so it extends correctly)
-  spring = sprite_data(sprite_id_location(10, 4), tile_vector(2, 1), vector(10, 2), colors.pink),
-  spring_extended = sprite_data(sprite_id_location(10, 5), tile_vector(2, 2), vector(10, 10), colors.pink)
-}
+    -- emerald representation tile (left part) and object sprite (both parts)
+    emerald                             = {   10, 15,    1, 1,     3, 2,       colors.pink},
+
+    -- spring (pivot at bottom center on both sprites so it extends correctly)
+    spring                              = {    10, 4,    2, 1,   10,  2,       colors.pink},
+    spring_extended                     = {    10, 5,    2, 2,   10, 10,       colors.pink}
+  },
+  function (params)
+    return sprite_data(sprite_id_location(params[1], params[2]), tile_vector(params[3], params[4]), vector(params[5], params[6]), params[7])
+  end
+)
 
 -- derived data: the representative sprite of an emerald (the one placed on the tilemap)
 --  in the left part of the sprite, so convert id location (which is at top-left) to sprite ID

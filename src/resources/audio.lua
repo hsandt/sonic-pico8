d6cbe14291904e3c022645cc56180719022de044
huulong <n.huu.long@gmail.com>
2021-08-28 14:21:09 +0200

    [AUDIO] SFX: added emerald_fly as SFX 49 (experiments in 48, experimental instruments in 1-2-3)

--- src/resources/audio.lua
+++ src/resources/audio.lua
@@ -2,6 +2,18 @@
 --(when using replace_strings with --game-constant-module-path [this_data.lua], all namespaced constants
 -- below are replaced with their values (as strings), so this file can be skipped)
 
+-- audio usage ranges
+-- menu
+-- kinda freestyle, but currently:
+-- use 8-31 for music tracks (in practice, stops at 26)
+-- use 32-63 for sound effects
+-- ingame
+-- use 8-53 for music tracks
+-- use 54-63 for sound effects
+-- stage clear
+-- use 8-15 for music tracks
+-- use 16-63 for sound effects
+
 local audio = {}
 
 -- OPTIMIZE CHARS: we can split menu, stage clear and ingame sound data to reduce size of ingame cartridge
@@ -9,6 +21,7 @@ local audio = {}
 audio.sfx_ids = {
 
   -- builtin_data_titlemenu and builtin_data_stage_clear only
+  emerald_fly = 49,  -- inspired by super emerald sound B8 in Sonic 3 & K
   menu_select = 50,  -- currently no sound, as Sonic 3 didn't have one either
   menu_confirm = 51,
 

37b2a345e9027cab833f0201694639626a0e7722
huulong <n.huu.long@gmail.com>
2021-08-29 20:19:28 +0200

    [TEST] Fixed tests on emerald cinematic, and stage_clear/stage_state in general by removing unused dependency to titlemenu (tests were failing on require since some visual titlemenu addon constants were required in outer scope; but since then we've been splitting the game in cartridges, so we're not loading titlemenu as a new state anymore, just loading a new cartridge; so no dependency anymore)

--- src/ingame/stage_state_utest.lua
+++ src/ingame/stage_state_utest.lua
@@ -1,9 +1,4 @@
 require("test/bustedhelper_ingame")
--- we should only need common_ingame required in bustedhelper_ingame,
---  but exceptionally we have titlemenu-related tests in this file, so we need stuff
---  like fun_helper (we should actually isolate tests and reverse cross-testing to itests,
---  whether complex tests done via busted but done in dedicated files, or simulation tests)
-require("common_titlemenu")
 
 local stage_state = require("ingame/stage_state")
 

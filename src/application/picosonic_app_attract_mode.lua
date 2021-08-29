ba60672bea935587662c449f8a8603ab789b2511
huulong <n.huu.long@gmail.com>
2021-08-29 20:43:42 +0200

    [ATTRACT MODE] Fixed attract mode not returning to titlemenu since the main -> app move Now using proper method accessed via self to access app in sequence async Added itestattractmode to catch such issues with Travis earlier

--- src/application/picosonic_app_attract_mode.lua
+++ src/application/picosonic_app_attract_mode.lua
@@ -4,6 +4,7 @@
 -- this really only defines used gamestates
 --  and wouldn't be necessary if we injected gamestates from main scripts
 
+local flow = require("engine/application/flow")
 local picosonic_app_base = require("application/picosonic_app_base")
 
 local input = require("engine/input/input")
@@ -11,10 +12,72 @@ local postprocess = require("engine/render/postprocess")
 
 local stage_state = require("ingame/stage_state")
 
+local picosonic_app_attract_mode = derived_class(picosonic_app_base)
+
+function picosonic_app_attract_mode:instantiate_gamestates() -- override (mandatory)
+  return {stage_state()}
+end
+
+function picosonic_app_attract_mode:on_post_start() -- override (optional)
+  picosonic_app_base.on_post_start(self)
+
+  -- postprocess for fade-out
+  -- usually this is done in gamestate, but attract mode uses the same stage_state
+  --  as ingame, so we'd need #if attract_mode to distinguish behavior, and it's simpler to
+  --  put dedicated behavior here
+  self.postproc = postprocess()
+  -- self.is_exiting = false  -- commented out to spare characters, as nil is equivalent
+
+  menuitem(5, "back to title", function()
+    -- prefer passing basename for compatibility with .p8.png
+    load('picosonic_titlemenu')
+  end)
+
+  -- start attract mode scenario
+  self:start_coroutine(self.attract_mode_scenario_async, self)
+end
+
+function picosonic_app_attract_mode:on_update() -- override (optional)
+  -- handle input: press any button to leave attract mode (avoids entering pause menu)
+  -- usually inputs are handled in gamestate, but for same reason as self.postproc we do it here
+
+  if input:is_just_pressed(button_ids.o) or input:is_just_pressed(button_ids.x) then
+    self:exit_attract_mode()
+  end
+
+--#if recorder
+  -- increment total frames for timed recording
+  total_frames = total_frames + 1
+--#endif
+end
+
+function picosonic_app_attract_mode:on_render() -- override (render)
+  self.postproc:apply()
+end
+
+function picosonic_app_attract_mode:exit_attract_mode()
+  if not self.is_exiting then
+    self.is_exiting = true
+    self:start_coroutine(self.exit_attract_mode_async, self)
+  end
+end
+
+function picosonic_app_attract_mode:exit_attract_mode_async()
+  -- fade out
+  for i = 1, 5 do
+    self.postproc.darkness = i
+    yield_delay_frames(6)
+  end
+
+  printh("load titlemenu!")
+  -- prefer passing basename for compatibility with .p8.png
+  load('picosonic_titlemenu')
+end
+
 -- define everything Sonic must do during the attract mode here
 -- it's not 100% correct to do it in main because this relies on flow having entered stage state
 --  but since it's the initial state, it should be entered on frame 1 anyway
-local function attract_mode_scenario_async()
+function picosonic_app_attract_mode:attract_mode_scenario_async()
   -- wait for 1 frame so flow finishes loading the initial state: stage
   yield()
 
@@ -151,68 +214,7 @@ local function attract_mode_scenario_async()
   -- total_frames = 0  -- reset total frames as we want relative delays since last record
 
   -- end demo, exit attract mode with fade out (if not already due to input)
-  app:exit_attract_mode()
-end
-
-local picosonic_app_attract_mode = derived_class(picosonic_app_base)
-
-function picosonic_app_attract_mode:instantiate_gamestates() -- override (mandatory)
-  return {stage_state()}
-end
-
-function picosonic_app_attract_mode:on_post_start() -- override (optional)
-  picosonic_app_base.on_post_start(self)
-
-  -- postprocess for fade-out
-  -- usually this is done in gamestate, but attract mode uses the same stage_state
-  --  as ingame, so we'd need #if attract_mode to distinguish behavior, and it's simpler to
-  --  put dedicated behavior here
-  self.postproc = postprocess()
-  -- self.is_exiting = false  -- commented out to spare characters, as nil is equivalent
-
-  menuitem(5, "back to title", function()
-    -- prefer passing basename for compatibility with .p8.png
-    load('picosonic_titlemenu')
-  end)
-
-  -- start attract mode scenario
-  self:start_coroutine(attract_mode_scenario_async)
-end
-
-function picosonic_app_attract_mode:on_update() -- override (optional)
-  -- handle input: press any button to leave attract mode (avoids entering pause menu)
-  -- usually inputs are handled in gamestate, but for same reason as self.postproc we do it here
-
-  if input:is_just_pressed(button_ids.o) or input:is_just_pressed(button_ids.x) then
-    self:exit_attract_mode()
-  end
-
---#if recorder
-  -- increment total frames for timed recording
-  total_frames = total_frames + 1
---#endif
-end
-
-function picosonic_app_attract_mode:on_render() -- override (render)
-  self.postproc:apply()
-end
-
-function picosonic_app_attract_mode:exit_attract_mode()
-  if not self.is_exiting then
-    self.is_exiting = true
-    self:start_coroutine(self.exit_attract_mode_async, self)
-  end
-end
-
-function picosonic_app_attract_mode:exit_attract_mode_async()
-  -- fade out
-  for i = 1, 5 do
-    self.postproc.darkness = i
-    yield_delay_frames(6)
-  end
-
-  -- prefer passing basename for compatibility with .p8.png
-  load('picosonic_titlemenu')
+  self:exit_attract_mode()
 end
 
 return picosonic_app_attract_mode
